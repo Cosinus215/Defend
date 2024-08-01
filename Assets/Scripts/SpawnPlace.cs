04951dcf73ff08e941c718d17eb9bafca9535cf2
Cosinus215 <Cosinus215@gmail.com>
2024-08-01 22:46:53 +0200

    Change UnitSpawn from MonoBehaviour to ScriptableObject

---
+++ Assets/Scripts/SpawnPlace.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpawnPlace", menuName = "SpawnPlace/new spawn place")]
+public class SpawnPlace : ScriptableObject {
+    public Transform spawnPosition;
+
+
+}
--- Assets/Scripts/Unit/UnitSpawn.cs
+++ Assets/Scripts/Unit/UnitSpawn.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitSpawn : MonoBehaviour {
-    [SerializeField] private Transform unitsParent;
+[CreateAssetMenu(fileName = "Unit", menuName = "Units/New Unit")]
+public class UnitSpawn : ScriptableObject {
+    [SerializeField] private GameObject unit;
+    [SerializeField] private SpawnPlace spawnPlace;
 
-    public void CreateUnit(GameObject unit) {
-        Instantiate(unit, unitsParent);
+    public void CreateUnit() {
+        Instantiate(unit, spawnPlace.spawnPosition.position, Quaternion.identity);
     }
 }
