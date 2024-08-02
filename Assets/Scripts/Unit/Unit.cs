9f93ca20f61568b2a3e1fda4ed3a4d6668f49a1d
Cosinus215 <Cosinus215@gmail.com>
2024-08-02 22:09:28 +0200

    Change unit movement from physics to transform

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
     private Rigidbody2D rb;
     public float speed;
+    public bool isAttacking;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate() {
-        rb.velocity = Vector2.right * speed;
+    private void Update() {
+        if (isAttacking) 
+            return;
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
+
+    
 }
